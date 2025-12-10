bc14f5df41d65ac3a8bca595bb05f7df82465731
DannKes <125414114+DannKes@users.noreply.github.com>
2025-12-10 14:22:42 -0400

    Daniel cristian (#9)
    * ultimo ajuste

--- FilmesApp/Services/TMDb/ITmdbApiService.cs
+++ FilmesApp/Services/TMDb/ITmdbApiService.cs
@@ -3,10 +3,7 @@ using FilmesApp.Models.TmdbDtos;
 
 namespace FilmesApp.Services.TMDb
 {
-    /// <summary>
-    /// Interface do cliente TMDb desacoplado (RF05).
-    /// Define os métodos obrigatórios: SearchMoviesAsync, GetMovieDetailsAsync, GetMovieImagesAsync, GetConfigurationAsync.
-    /// </summary>
+    
     public interface ITmdbApiService
     {
         Task<SearchResultDto?> SearchMoviesAsync(string query, int page = 1);
